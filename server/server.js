e99f9cc8a998ca64112df0a40ce90821a98bf60b
ShuvroSankar <sankarshuvro1010@gmail.com>
2023-09-10 18:22:50 +0600

    product api not complete

--- server/server.js
+++ server/server.js
@@ -5,8 +5,9 @@ require('dotenv').config()
 const dbConfig = require('./config/dbConfig')
 const port = process.env.PORT || 5000
 const useRoute = require("./routes/useRoute")
-
+const productRoute = require("./routes/productsRoute")
 
 app.use("/api/users", useRoute)
+app.use("api/products", productRoute)
 
 app.listen(port, () => { console.log(`We are listening Node/Express server on ${port}`) });
